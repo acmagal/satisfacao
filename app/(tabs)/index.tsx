21a70a67082c8cc85e37c0e13ccc7108df1c10ed
acmagal <andre.emprego99@gmail.com>
2025-03-13 15:22:41 -0300

    mock

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,74 +1,163 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Image, StyleSheet, Platform, View, Text, Button, TouchableOpacity } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+type AnswerState = {
+  optA: number,
+  optB: number,
+  optC: number,
+  optD: number
+}
 
 export default function HomeScreen() {
+
+  const [answers, setAnswers] = useState<AnswerState>({
+    optA: 0,
+    optB: 0,
+    optC: 0,
+    optD: 0,
+  });
+
+
+  const storeData = async (value: AnswerState) => {
+    try {
+      const jsonValue = JSON.stringify(value);
+      await AsyncStorage.setItem('my-key', jsonValue);
+    } catch (e) {
+      // saving error
+    }
+  };
+
+  const getData = async () => {
+    try {
+      const jsonValue = await AsyncStorage.getItem('my-key');
+      return jsonValue != null ? JSON.parse(jsonValue) : null;
+    } catch (e) {
+      // error reading value
+    }
+  };
+
+
+  useEffect(() => {
+    const results = getData();
+    console.log(results);
+
+  }, []);
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text>
+          primeira pergunta
+        </Text>
+        <Text>
+          resposta {JSON.stringify(answers)}
+        </Text>
+      </View>
+      <View style={styles.actions}>
+        <TouchableOpacity
+          onPress={
+            () => {
+              setAnswers(
+                (previous) => ({
+                  ...previous, optA: previous.optA + 1
+                })
+              )
+              storeData(answers);
+            }
+          }
+          style={styles.button}>
+          <Text>
+            OpçãoA
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={
+            () => {
+              setAnswers(
+                (previous) => ({
+                  ...previous, optB: previous.optB + 1
+                })
+              )
+              storeData(answers);
+            }
+          } style={styles.button}>
+          <Text>
+            OpçãoB
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={
+            () => {
+              setAnswers(
+                (previous) => ({
+                  ...previous, optC: previous.optC + 1
+                })
+              )
+              storeData(answers);
+            }
+          } style={styles.button}>
+          <Text>
+            OpçãoC
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={
+            () => {
+              setAnswers(
+                (previous) => ({
+                  ...previous, optD: previous.optD + 1
+                })
+              )
+              storeData(answers);
+            }
+          } style={styles.button}>
+          <Text>
+            OpçãoD
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
+  container: {
+    backgroundColor: "pink",
+    height: "100%",
+
+  },
+  header: {
+    backgroundColor: "orange",
+    flex: 5,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  actions: {
+
+    flex: 3,
     flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
     alignItems: 'center',
-    gap: 8,
+    width: '100%',
+    marginBottom: 10,
+    gap: 50,
+    paddingTop: 10,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
+
+  button: {
+
+    width: '25%', // 4 columns
+    aspectRatio: 1, // Makes it a square
+    backgroundColor: 'blue',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: 'white',
+    borderRadius: 8,
+
+  }
+
 });
